e289b169ee2f88a487b5bf6c28ade1338716b6f6
Yeganeh <yeganeh.bakhshi.at@gmail.com>
2025-05-10 17:20:17 +0330

    Craete MultiBlobContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { MultiBlobContainer } from "./components/blob/MultiBlobContainer";
 import BlobMover0 from "./components/BlobMover0";
 import BlobMover1 from "./components/BlobMover1";
 import BlobMover3 from "./components/BlobMover3";
@@ -7,10 +8,11 @@ import Home from "./components/Home";
 export default function App() {
   return (
     <>
-      <BlobMover0 />
+      {/* <BlobMover0 />
       <BlobMover1 />
       <BlobMover2 />
-      <BlobMover3 />
+      <BlobMover3 /> */}
+      <MultiBlobContainer/>
       <div className="w-full h-screen flex items-center justify-center z-10">
         <Home />
       </div>
